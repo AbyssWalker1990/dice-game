bc5f1bf2bb6778918fb0a8a88a2be5cce6e59af4
Vova <bordiian.v@gmail.com>
2023-04-23 13:13:34 +0300

    added method for creating custom sides dice Sets

--- src/Player.ts
+++ src/Player.ts
@@ -1,9 +1,13 @@
+import Dice from "./Dice"
+
 export default class Player {
   constructor(
     public name: string, 
+    public diceSet: Set<Dice>,
     private result: number = 0,
     private round: number = 1,
     private isChampion: boolean = false) {
       this.name = name
+      this.diceSet = diceSet
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,43 @@ import Dice from "./Dice"
 import Player from "./Player"
 
 class GameService {
-  constructor(public playersArr: string[], public diceSides = 6) {
+  private diceSet: Set<Dice>
+  constructor(public playersArr: string[], public diceAmount = 2, public diceSides = 6) {
     this.playersArr = playersArr
+    this.diceAmount = diceAmount
     this.diceSides = diceSides
+    this.diceSet = this.createDiceSet()
   }
 
   public showPlayers = (): void => {
-    const players = this.createPlayers()
+    const players = this.createPlayers(this.diceSet)
     players.forEach(player => console.log(player))
   }
 
-  private createPlayers = (): Player[] => {
+  public showDiceSet = (): void => {
+    const diceSet = this.createDiceSet()
+    diceSet.forEach(dice => console.log(dice))
+  }
+
+  private createPlayers = (diceSet: Set<Dice>): Player[] => {
     const players: Player[] = []
     this.playersArr.forEach(playerName => {
-      const player = new Player(playerName)
+      const player = new Player(playerName, diceSet)
       players.push(player)
     })
     return players
+  }
+
+  private createDiceSet = (): Set<Dice> => {
+    const diceSet: Set<Dice> = new Set()
+    for (let i = 0; i < this.diceAmount; i++) {
+      const dice = new Dice(this.diceSides)
+      diceSet.add(dice)
+    }
+    return diceSet
   }
 }
 
 const gameService = new GameService(['Bob', 'Dylan'])
 gameService.showPlayers()
+gameService.showDiceSet()
